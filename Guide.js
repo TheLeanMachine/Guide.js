ff65be6574e6ce1bbc68f15f213ca3d688c7cb7a
Kai Hoelscher <myfatbackup@yahoo.de>
2013-03-03 23:34:07 +0100

    Bugfix: Use renderService as Singleton

--- Guide.js
+++ Guide.js
@@ -6,7 +6,6 @@
  * Licensed under the MIT license.
  */
 
-// TODO: [BUG] Adding a Guide multiple times adde multiple DOM nodes(!!!)
 // TODO: [BUG] what happens if an un-activaed Guide gets deaktivated?
 // TODO: [BUG] why does 'activateAll()' does not get calles when button clicked?
 // >>> TODO: [BUG] guidejs_test: a) why does null check fail? b) what kind of error is thrown if no guideConfig is provided?
@@ -18,11 +17,11 @@
 // TODO: [FEATURE] New Guide type: GuidedTour() ...at first, just a collection of Guiders
 // TODO: [FEATURE] Implement DefaultRenderAdapter that natively renders the helpbox (via HTML API?) ????
 // >>> TODO: [REFACTOR] renderHtmTo() [and subroutines!]: split in sub-modules etc. DomService(), EventService(), TaskService()
-// TODO: [REFACTOR] Imporove performace of methods like debugEnabled() oder renderer()
 // TODO: [REFACTOR] add Guide in DOM as child nodes(instead of sibling), make parent "position: relative;" and use this as starting point for rendering
 // TODO: [REFACTOR] Rename HelpBoxGuide to HelpBox (???)
 // TODO: [REFACTOR] make use of renderAdapter()
 // TODO: [REFACTOR] expose concrete 'classes' instead of generic 'newGuide()' method: HelpBox, GuidedTour,...
+// TODO: [REFACTOR] Improve performance of methods like debugEnabled() oder renderer() (???)
 // TODO: [VALIDATION] Args of createHelpBoxGuide() -> set to reasonable defaults otherwise
 (function (undefined) { // we always get 'undefined' here, since this code is directly invoked without arguments
 
@@ -45,6 +44,7 @@
 
   var _guides = []; // all Guides created by this lib
   var _lastAddedGuideId = 0; // incremented when Guide is created
+  var _libCache = {};
 
 
   /**
@@ -100,19 +100,18 @@
       var fadeOutMillis = guideConfig.fadeOutMillis;
       var displayDuration = guideConfig.displayDuration;
 
-
       var anchor = debugModeEnabled() ? DEBUG_URL_HASH : 'top';
       var html = '<div id="'+ helpBoxCssId +'" class="helpBox">' + content + '<br><a id="'+ closeLinkCssId +'" href="#'+ anchor +'">close</a></div>';
-      renderer().renderHtmlTo(targetCssId, html, helpBoxCssId, displayDuration, fadeOutMillis);
+      renderService().renderGuideTo(targetCssId, html, helpBoxCssId, displayDuration, fadeOutMillis);
 
-      renderer().attachEventTo('click', closeLinkCssId, function() {
+      renderService().attachEventTo('click', closeLinkCssId, function() {
         deactivate();
       });
     }
 
     // TODO add doc
     function deactivate() {
-      renderer().hide(helpBoxCssId);
+      renderService().hide(helpBoxCssId);
     }
 
     function isLoaded() {
@@ -125,19 +124,26 @@
   }
 
   // TODO add doc (rename to wrapJQuery() )
-  function JQueryRenderAdapter($) {
+  function JQueryRenderService($) {
+    var renderedGuides = {};
+
     // TODO add doc
-    function renderHtmlTo(cssIdRenderTarget, html, cssIdGuideContainer, displayDuration, fadeOutMillis) {
+    function renderGuideTo(cssIdRenderTarget, html, cssIdGuideContainer, displayDuration, fadeOutMillis) {
 
 // domService().insertHtml(...)
       // TODO a) client should add plain CSS-ID b) write addHtmlAsChildOf() method
-      $('#' + cssIdRenderTarget).prepend(html);
+      if (!renderedGuides[cssIdGuideContainer]) {
+        $('#' + cssIdRenderTarget).prepend(html);
+        renderedGuides[cssIdGuideContainer] = 'dummy';
+      } else {
+        $('#' + cssIdGuideContainer).show();
+      }
 
 // eventService().scheduleFor(delayMillis, fn)
       setTimeout(function() {
         $('#' + cssIdGuideContainer).fadeOut(fadeOutMillis);
       }, displayDuration);
-      
+
       logDebug('Rendered Guide with CSS-ID "'+ cssIdGuideContainer +'" to element with CSS-ID "'+ cssIdRenderTarget +'"');
     }
 
@@ -150,7 +156,7 @@
       $('#' + cssIdGuideContainer).on(eventName, fn);
     }
 
-    this.renderHtmlTo = renderHtmlTo;
+    this.renderGuideTo = renderGuideTo;
     this.hide = hide;
     this.attachEventTo = attachEventTo;
   }
@@ -174,10 +180,16 @@
   }
 
   // TODO doc
-  function renderer() {
-    if (jQueryAvailable()) {
-      return new JQueryRenderAdapter(GLOBAL_CONTEXT.jQuery);
+  function renderService() {
+    // TODO: use lib cache
+    if (_libCache['renderService']) {
+      return _libCache['renderService'];
     }
+
+    if (jQueryAvailable()) {
+      return _libCache['renderService'] = new JQueryRenderService(GLOBAL_CONTEXT.jQuery);
+    }
+
     logError('Cannot render any Guides: No appropriate lib found in global context.');
     return null;
   }
